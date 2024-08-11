00a7aa6cf76116acade870cc9d309c9af4434e1e
DavidDaoTu <tudv.mechatronics.hust@gmail.com>
2024-08-11 15:24:33 +0700

    Completed the project

--- models.py
+++ models.py
@@ -22,9 +22,9 @@ class Net(nn.Module):
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        # the output_size of each feature map: (W-F)/S+1 = (244-5)/1+1 = 240
-        # output tensor size: (32, 240, 240)
-        # tensor size after pooling: (32, 120, 120)
+        # the output_size of each feature map: (W-F)/S+1 = (224-5)/1+1 = 220
+        # output tensor size: (32, 220, 220)
+        # tensor size after pooling: (32, 110, 110)
         self.conv1 = nn.Conv2d(1, 32, 5) 
         
         ## Note that among the layers to add, consider including:
@@ -32,22 +32,46 @@ class Net(nn.Module):
         # output tensor size after pooling: (out_channel, img_size/2, img_size/2)
         self.pool = nn.MaxPool2d(2, 2) 
         
-        # multiple conv layers: with kernel_size=5
-        # the output size of each feature map: (W-F)/S+1 = (120-5)/1+1=116
-        # output tensor size: (64, 116, 116)
-        # tensor size after pooling: (64, 58, 58)
+        # multiple conv layers: 
+        # Convolutional layer 2: with kernel_size=5
+        # the output size of each feature map: (W-F)/S+1 = (110-5)/1+1=106
+        # output tensor size: (64, 106, 106)
+        # tensor size after pooling: (64, 53, 53)
         self.conv2 = nn.Conv2d(32, 64, 5)
         
+        # Convolutional layer 3: with kernel_size=3
+        # the output size of each feature map: (W-F)/S+1 = (53-3)/1+1=51
+        # output tensor size: (128, 51, 51)
+        # tensor size after pooling: (128, 25, 25)
+        self.conv3 = nn.Conv2d(64, 128, 3)
+        
+        # Convolutional layer 4: with kernel_size=3
+        # the output size of each feature map: (W-F)/S+1 = (25-3)/1+1=23
+        # output tensor size: (256, 23, 23)
+        # tensor size after pooling: (256, 11, 11)
+        self.conv4 = nn.Conv2d(128, 256, 3)
+        
+        # Convolutional layer 5: with kernel_size=3
+        # the output size of each feature map: (W-F)/S+1 = (11-3)/1+1=9
+        # output tensor size: (512, 9, 9)
+        # tensor size after pooling: (512, 4, 4)
+        self.conv5 = nn.Conv2d(256, 512, 3)
+        
         # fully-connected layers:
-        # Input feature size: 64 * (58*58) (filtered then pooled map size)
-        self.fc1 = nn.Linear(64*58*58, 136)
+        # Input feature size: 512 * (4*4) (filtered then pooled map size)
+        self.fc1 = nn.Linear(512*4*4, 2048)
         
         # other layers (such as dropout or batch normalization) to avoid overfitting
-        # Dropout with p=0.4
-        self.fc1_drop = nn.Dropout(p=0.4)
+        # Dropout with p=0.21
+        self.fc_drop = nn.Dropout(p=0.21)
+        
+        # other linear class
+        self.fc2 = nn.Linear(2048, 1024)
+        self.fc3 = nn.Linear(1024, 512)
+        self.fc4 = nn.Linear(512, 256)
         
         # Finally, create output channels for N_CLASSES
-        self.fc2 = nn.Linear(136, Net.N_CLASSES)
+        self.fc5 = nn.Linear(256, Net.N_CLASSES * 2)
         
 
         
@@ -58,14 +82,27 @@ class Net(nn.Module):
         x = self.pool(F.relu(self.conv1(x)))
         # Passing the second conv layer + pool
         x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = self.pool(F.relu(self.conv4(x)))
+        x = self.pool(F.relu(self.conv5(x)))
         
         # Flatenning before passing through the fully-connected layer
         x = x.view(x.size(0), -1)
         
-        # Passing through 02 linear layers with dropout in between
+        # Passing through 03 linear layers with dropout in between
         x = F.relu(self.fc1(x))
-        x = self.fc1_drop(x)
-        x = self.fc2(x)
+        x = self.fc_drop(x)
+        
+        x = F.relu(self.fc2(x))
+        x = self.fc_drop(x)
+        
+        x = F.relu(self.fc3(x))
+        x = self.fc_drop(x)
+        
+        x = F.relu(self.fc4(x))
+        x = self.fc_drop(x)
+        
+        x = self.fc5(x)
         
         # a modified x, having gone through all the layers of your model, should be returned
         return x
